8f2076cc27d23c96795c45f825c474e544f5d815
Dan Vingo <dan@danvingo.com>
2023-01-14 10:03:42 -0500

    Generate form inputs from schema

--- src/app/malli_react_hook_form/entry.cljs
+++ src/app/malli_react_hook_form/entry.cljs
@@ -14,9 +14,27 @@
 
 (def form-schema
   [:map
-   [:username [:string {:min 4 :max 15}]]
-   [:telephone [:re {:error/message "Please enter a valid phone number"}
-                #"^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}"]]])
+   [:username {:ui-form/label "Username"
+               :ui-form/component form/ui-form-input}
+    [:string {:min 4 :max 15}]]
+   [:telephone {:ui-form/label "Telephone num"
+                :ui-form/placeholder "(913)-345-2303"
+                :ui-form/component form/ui-telephone-input}
+    [:re {:error/message "Please enter a valid phone number"}
+     #"^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}"]]])
+
+(defn map-schema->inputs [schema on-change]
+  (->>
+   (m/children schema)
+   (map (fn [[k opts]]
+          (let [{:ui-form/keys [placeholder label component]
+                 :or {placeholder "" label ""
+                      component form/ui-form-input}} opts]
+            (d/div {:key (name k)}
+                   ($ component {:placeholder placeholder
+                                 :label       label
+                                 :on-change  #(on-change k %)
+                                 :name        (name k)})))))))
 
 (defn use-malli-resolver [schema]
   (let [validator (m/validator schema)
@@ -28,50 +46,54 @@
           (if valid?
             #js{:values data :errors #js{}}
             (let [errs (reduce-kv
-                         (fn [errs field messages]
-                           (doto errs
-                             (g/set (name field) #js{:type "validation" :message (first messages)})))
-                         #js{}
-                         (me/humanize (explain data)))]
+                        (fn [errs field messages]
+                          (doto errs
+                            (g/set (name field)
+                              #js{:type "validation" :message (first messages)})))
+                        #js{}
+                        (me/humanize (explain data)))]
               #js{:values #js{} :errors errs})))))))
 
 (defnc app []
   (let [resolver      (use-malli-resolver form-schema)
         form-methods  (useForm #js{:resolver resolver})
         handle-submit (.-handleSubmit form-methods)
+        on-change     (hooks/use-callback [] (fn [k v] (.log js/console "CHANGE: " k ", " v)))
         on-submit     (hooks/use-callback [] (fn [data] (.log js/console "SUBMIT: " data)))]
     ($ FormProvider {:& form-methods}
-      (d/div
+       (d/div
         (d/pre (with-out-str (pprint form-schema)))
         (d/form {:onSubmit (handle-submit on-submit)}
-
           (d/div {:style
                   {:width           "40%" :display "flex" :margin-bottom "20px"
-                   :justify-content "space-between"
-                   :padding         "20px" :border "1px solid"}}
-            (d/div
-              ($ form/ui-form-input {:placeholder ""
-                                     :label       "username"
-                                     :name        "username"}))
+                    :justify-content "space-between"
+                    :padding         "20px" :border "1px solid"}}
+                  (map-schema->inputs form-schema on-change)
+                  ;; alternatively, the fields can be defined statically:
+                  ;; (d/div
+                  ;;  ($ form/ui-form-input {:placeholder ""
+                  ;;                         :label       "username"
+                  ;;                         :name        "username"}))
 
-            (d/div
-              ($ form/ui-form-input {:placeholder "(913)-345-2303"
-                                     :label       "Telephone num"
-                                     :name        "telephone"})))
+                  ;; (d/div
+                  ;;  ($ form/ui-form-input {:placeholder "(913)-345-2303"
+                  ;;                         :label       "Telephone num"
+                  ;;                         :type "tel"
+                  ;;                         :name        "telephone"}))
+                  )
 
           (d/button {:type "submit" :style {:padding    "10px 20px"
                                             :color      "oldLace"
                                             :background "hsl(300deg 60% 60%)"}} "Save")
 
           (d/button {:style    {:padding "10px 20px" :margin-left "60px"}
-                     :on-click (fn [e]
-                                 (.preventDefault e)
-                                 (.reset form-methods))} "Reset"))))))
+                      :on-click (fn [e]
+                                  (.preventDefault e)
+                                  (.reset form-methods))} "Reset"))))))
 
 (defonce root (react-dom/createRoot (js/document.getElementById "app")))
 
 (defn ^:export init []
-  (println "INIT!")
   (.render root ($ react/StrictMode ($ app))))
 
 (defn ^:dev/after-load refresh [] (.render root ($ react/StrictMode ($ app))))
--- src/app/malli_react_hook_form/form.cljs
+++ src/app/malli_react_hook_form/form.cljs
@@ -1,5 +1,6 @@
 (ns app.malli-react-hook-form.form
   (:require
+    [clojure.string :as str]
     [helix.core :as h :refer [<> $ fnc]]
     [helix.dom :as d]
     [goog.object :as g]
@@ -8,13 +9,13 @@
 
 (def base-input
   (react/forwardRef
-    (fnc base-input [{:keys [label error] :as props} input-ref]
-      (let [input-props (dissoc props :label :error)]
-        (d/div
+   (fnc base-input [{:keys [label error] :as props} input-ref]
+        (let [input-props (dissoc props :label :error)]
           (d/div
+           (d/div
             (when label (d/label label))
             (d/input {:& input-props :ref input-ref}))
-          (when error (d/div {:style {:marginTop "10px" :color "hsl(341deg 94% 43%)"}} error)))))))
+           (when error (d/div {:style {:marginTop "10px" :color "hsl(341deg 94% 43%)"}} error)))))))
 
 ;; using forwardRef we can still use react refs from the calling code if we wish
 
@@ -25,38 +26,50 @@ the component from your favorite library.
 "
 (def ui-form-input
   (react/forwardRef
-    (fnc base-input
-      [{:keys [default-value on-change] :as props
-        :or   {on-change identity}}
-       input-ref]
-      (let [default-value (or default-value "")
-            form-context  (useFormContext)]
-        (assert form-context "Missing form context for ui-form-input")
-        (assert (:name props) "Missing 'name' context for ui-form-input")
+   (fnc base-input
+        [{:keys [default-value on-change on-change-xf] :as props
+          :or   {on-change identity
+                 on-change-xf identity}}
+         input-ref]
+        (let [default-value (or default-value "")
+              form-context  (useFormContext)]
+          (assert form-context "Missing form context for ui-form-input")
+          (assert (:name props) "Missing 'name' context for ui-form-input")
 
-        (let [input-props (dissoc props :default-value :on-change)
-              control     (.-control form-context)
-              controller  (useController #js{:name         (:name props)
-                                             :control      control
-                                             :rules        (:rules props)
-                                             :defaultValue default-value})
-              field       (.-field controller)
-              field-state (.-fieldState controller)
-              invalid?    (g/get field-state "invalid")
-              error-msg   (and invalid? (g/getValueByKeys field-state "error" "message"))]
-          (d/div
-            (d/pre "touched: " (pr-str (g/get field-state "isTouched")))
-            (d/pre "dirty: " (pr-str (g/get field-state "isDirty")))
-            (d/pre "invalid: " (pr-str (g/get field-state "invalid")))
-            ($ base-input
-              {:&         input-props
-               :error     error-msg
-               :on-change (fn [e]
-                            (on-change e)
+          (let [input-props (dissoc props :default-value :on-change :on-change-xf)
+                control     (.-control form-context)
+                controller  (useController #js{:name         (:name props)
+                                               :control      control
+                                               :rules        (:rules props)
+                                               :defaultValue default-value})
+                field       (.-field controller)
+                field-state (.-fieldState controller)
+                invalid?    (g/get field-state "invalid")
+                error-msg   (and invalid? (g/getValueByKeys field-state "error" "message"))]
+            (d/div
+             (d/pre "touched: " (pr-str (g/get field-state "isTouched")))
+             (d/pre "dirty: " (pr-str (g/get field-state "isDirty")))
+             (d/pre "invalid: " (pr-str (g/get field-state "invalid")))
+             ($ base-input
+                {:&         input-props
+                 :error     error-msg
+                 :on-change (fn [e]
+                              (let [value (on-change-xf e)]
+                                (on-change value)
                             ;; this also works, you can for example parse the string to send to react hook form
                             ;; for date or number input etc.
                             ;(.onChange field (.. e -target -value))
-                            (.onChange field e))
-               :on-blur   (.-onBlur field)
-               :value     (.-value field)
-               :ref       (or input-ref (.-ref field))})))))))
+                                (.onChange field value)))
+                 :on-blur   (.-onBlur field)
+                 :value     (.-value field)
+                 :ref       (or input-ref (.-ref field))})))))))
+
+(defn filter-telephone-num [e]
+  (if (empty? (-> e .-target .-value))
+    ""
+    (str/join (filter (into #{\( \) \- \space} (map str (range 10))) (-> e .-target .-value)))))
+
+(def ui-telephone-input
+  (react/forwardRef
+   (fnc [args]
+        ($ ui-form-input {:& args :type "tel" :on-change-xf filter-telephone-num}))))
